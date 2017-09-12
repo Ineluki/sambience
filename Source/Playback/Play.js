480c9f8fc24d845f2bada795182bf62d8b5b86fc
Stormking <sturmspitze@googlemail.com>
2017-09-12 09:23:35 +0200

    changed playback

--- Source/Playback/Play.js
+++ Source/Playback/Play.js
@@ -1,5 +1,7 @@
-const player = require('play-sound')();
+const spawn = require('child_process').spawn;
 const debug = require('debug')('sambience');
+
+const player = require('../Util/Config.js').get('playback.player');
 
 const methods = {};
 
@@ -8,23 +10,28 @@ let audio;
 methods.start = function(file) {
 	methods.stop();
 	return new Promise(function(resolve, reject) {
-		let audioLocal;
-		audioLocal = audio = player.play(file, function(err) {
+		const closed = function(err) {
 			if (err && !err.killed) {
 				reject(err);
 			} else {
-				debug("stopped",audioLocal ? audioLocal.pid : 0, audio ? audio.pid : 0, audioLocal === audio ? 'same' : '');
+				debug("stopped",audioLocal ? audioLocal.pid : 0, audio ? audio.pid : 0,
+						audioLocal === audio ? 'same' : '');
 				if (audioLocal === audio) {
 					resolve();
 				}
 			}
+		};
+		let audioLocal;
+		audioLocal = audio = spawn(player, [file], {
+			stdio: 'ignore',
+			detached: false
 		});
+		audioLocal.on('close',closed);
 		debug("playing "+file,audio.pid);
 	});
 };
 
 methods.stop = function() {
-
 	if (audio) {
 		debug("stop play", audio ? audio.pid : 0);
 		audio.kill();
